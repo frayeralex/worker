edfeef9cf98570a80d98eb778445accfb0be5d18
Alex Boyko <frayeralex@gmail.com>
2019-11-19 18:26:31 +0200

    add MainActivity worker run/stop/show result implementation

--- app/src/main/java/com/github/frayeralex/worker/MainActivity.kt
+++ app/src/main/java/com/github/frayeralex/worker/MainActivity.kt
@@ -2,11 +2,65 @@ package com.github.frayeralex.worker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.work.*
+import com.github.frayeralex.worker.workers.PrimeCalculationWorker
+import kotlinx.android.synthetic.main.activity_main.*
+import androidx.lifecycle.Observer
 
 class MainActivity : AppCompatActivity() {
+    private val wm by lazy { WorkManager.getInstance(this) }
+    private var primeList = ArrayList<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun startCalculation(v: View) {
+        primeList.clear()
+        val work = OneTimeWorkRequestBuilder<PrimeCalculationWorker>()
+            .addTag(TAG)
+            .build()
+
+        wm.enqueue(work)
+
+        wm.getWorkInfoByIdLiveData(work.id).observe(this, Observer {
+            it?.progress?.getInt(PrimeCalculationWorker.PROGRESS, DEFAULT_VALUE)?.apply {
+                if (this != DEFAULT_VALUE && !primeList.contains(this)) {
+                    primeList.add(this)
+                    updateUi()
+                }
+            }
+        })
+    }
+
+    fun stopCalculation(v: View) {
+        wm.cancelAllWorkByTag(TAG)
+        primeList.clear()
+        updateUi()
+    }
+
+    override fun onDestroy() {
+        wm.cancelAllWork()
+        super.onDestroy()
+    }
+
+    private fun updateUi() {
+        if (primeList.isNotEmpty()) {
+            displayImage(primeList[primeList.size - 1].toString())
+        } else {
+            displayImage(resources.getString(R.string.app_name))
+        }
+    }
+
+    private fun displayImage(text: String) {
+        textView.text = text;
+
+    }
+
+    companion object {
+        private val TAG = "CALC_VALUE"
+        private val DEFAULT_VALUE = 1
     }
 }
